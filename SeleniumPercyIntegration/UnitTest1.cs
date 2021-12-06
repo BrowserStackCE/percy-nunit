9452c3751491c42884f292ac13379963ebfb1560
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2021-12-06 11:18:10 +0530

    Update UnitTest1.cs
    removed remote capabilities

--- SeleniumPercyIntegration/UnitTest1.cs
+++ SeleniumPercyIntegration/UnitTest1.cs
@@ -16,20 +16,6 @@ namespace SeleniumPercyIntegration
         [SetUp]
         public void Setup()
         {
-
-            //OpenQA.Selenium.Chrome.ChromeOptions capability = new OpenQA.Selenium.Chrome.ChromeOptions();
-            //capability.AddAdditionalCapability("os_version", "10", true);
-            //capability.AddAdditionalCapability("resolution", "1920x1080", true);
-            //capability.AddAdditionalCapability("browser", "Chrome", true);
-            //capability.AddAdditionalCapability("browser_version", "91.0", true);
-            //capability.AddAdditionalCapability("os", "Windows", true);
-            //capability.AddAdditionalCapability("name", "BStack-[C_sharp] Sample Test", true); // test name
-            //capability.AddAdditionalCapability("build", "BStack Build Number 1", true); // CI/CD job or build name
-            //capability.AddAdditionalCapability("browserstack.user", "mohammedk1", true);
-            //capability.AddAdditionalCapability("browserstack.key", "spBCpUJaVTnvxxssFtEJ", true);
-            //driver = new RemoteWebDriver(
-            //  new Uri("https://hub-cloud.browserstack.com/wd/hub/"), capability
-            //);
             driver = new ChromeDriver("/Users/mohammed/Downloads");
             _percy = new Percy();
         }
@@ -45,8 +31,7 @@ namespace SeleniumPercyIntegration
             Console.WriteLine(driver.Title);
             //_percy.Snapshot("HomePage", new Dictionary<string, object> { { "widths", new List<int> { 768, 992, 1200 } } });
             // _percy.Snapshot(driver, "Home Page", null);
-            _percy.Snapshot(driver,"a", new Dictionary<string, object> { { "widths", new List<int> { 768, 992, 1200 } } });
-            
+            _percy.Snapshot(driver,"a", new Dictionary<string, object> { { "widths", new List<int> { 768, 992, 1200 } } });  
             driver.Quit();
           
         }
